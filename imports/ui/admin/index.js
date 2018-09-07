47750a05382238f7f7e1ff69d342abb12d835631
Mrebai <35473407+Mrebai@users.noreply.github.com>
2018-09-07 04:56:27 +0100

    first commit

---
+++ imports/ui/admin/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Resource, Admin } from 'react-admin';
+
+class AdminIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { data } = this.props;
+    return (
+      <Admin dataProvider={data}>
+        <Resource name="Post" list={PostList} edit={PostEdit} create={PostCreate} />
+      </Admin>
+    );
+  }
+}
+AdminIndex.prototype = {
+  data: PropTypes.object,
+};
+
+export default AdminIndex;
